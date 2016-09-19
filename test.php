c9dd96ce1e8a944cb5f5ce1569474ebc39d3f245
YCJ-Aliyun <18092871256@163.com>
2016-09-19 15:57:24 +0800

    Start to Learn PHP

---
+++ test.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <?php
+      $server='localhost';
+      $user='root';
+      $passwd='ycj19950510';
+      $db='ycj';
+      $con=mysqli_connect($server,$user,$passwd);
+      $insert=mysqli_query($con,"insert into ycj.student value(0123456789,'YangChenjie',22);");
+      $delete=mysqli_query($con,'delete from ycj.student where id = 0510151318;');
+      $select=mysqli_query($con,'select * from ycj.student where id=123456789;');
+      $update=mysqli_query($con,'update ycj.student set age=25 where id=0123456789;');
+      echo var_dump($dd), '<br />';
+      if(!$insert){
+	echo 'Insert Error','<br />',mysqli_error($con);}
+      else{
+	echo 'Insert Down';}
+      
+      if(!$delete){
+        echo 'Delete Error','<br />',mysqli_error($con);}
+      else{
+        echo 'Delete Down';}
+
+      if(!$update){
+        echo 'Update Error','<br />',mysqli_error($con);}
+      else{
+        echo 'Update Down';}
+      if(!$select){
+        echo 'Select Error','<br />',mysqli_error($con);}
+      else{
+        echo 'Select Down','<br />',mysqli_fetch_assoc($select)['name'];}
+?>
+      </body>
+</html>
