b0306f602ca73f4369daa2e514c609ad37e8dd18
YangChenjie <18092871256@163.com>
2016-09-21 14:30:26 +0800

    login complete

--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<meta charset="UTF8">
+</head>
+<body>
 <?php
-	
+
+	$name = $_POST['name'];
+	$passwd = $_POST['passwd'];
+
+	// 链接数据库
+	$server = "127.0.0.1";
+	$user = "root";
+	$pswd = "";
+	$db = "ycj";
+	$conn = mysqli_connect($server ,$user ,$pswd , $db);
+
+	if($conn){		//数据库连接成功
+		$select_sql = "select passwd from vip_user where name ='" .$name ."'";
+		$select_result = mysqli_query($conn ,$select_sql );
+		$a = mysqli_fetch_assoc($select_result)["passwd"];
+
+		if ($passwd==$a) {
+			echo "<script>";
+			echo "window.location.href='test.php'";
+			echo "</script>";
+		}
+		else{
+			echo "<script>";
+			echo 'alert("账号或密码错误！");';
+			echo 'window.history.back()';
+			echo "</script>";
+		}
+	}
+	else 			//数据库连接失败
+		echo "数据库连接失败"
+		
 ?>
+</body>
+</html>
--- pages/test.php
+++ pages/test.php
@@ -1,35 +1,39 @@
 <!DOCTYPE html>
 <html>
+	<head>
+		<meta charset="UTF-8">
+	</head>
   <body>
     <?php
-      $server='localhost';
-      $user='root';
-      $passwd='ycj19950510';
-      $db='ycj';
-      $con=mysqli_connect($server,$user,$passwd);
-      $insert=mysqli_query($con,"insert into ycj.student value(0123456789,'YangChenjie',22);");
-      $delete=mysqli_query($con,'delete from ycj.student where id = 0510151318;');
-      $select=mysqli_query($con,'select * from ycj.student where id=123456789;');
-      $update=mysqli_query($con,'update ycj.student set age=25 where id=0123456789;');
-      echo var_dump($dd), '<br />';
-      if(!$insert){
-	echo 'Insert Error','<br />',mysqli_error($con);}
-      else{
-	echo 'Insert Down';}
+ //      $server='localhost';
+ //      $user='root';
+ //      $passwd='ycj19950510';
+ //      $db='ycj';
+ //      $con=mysqli_connect($server,$user,$passwd);
+ //      $insert=mysqli_query($con,"insert into ycj.student value(0123456789,'YangChenjie',22);");
+ //      $delete=mysqli_query($con,'delete from ycj.student where id = 0510151318;');
+ //      $select=mysqli_query($con,'select * from ycj.student where id=123456789;');
+ //      $update=mysqli_query($con,'update ycj.student set age=25 where id=0123456789;');
+ //      echo var_dump($dd), '<br />';
+ //      if(!$insert){
+	// echo 'Insert Error','<br />',mysqli_error($con);}
+ //      else{
+	// echo 'Insert Down';}
       
-      if(!$delete){
-        echo 'Delete Error','<br />',mysqli_error($con);}
-      else{
-        echo 'Delete Down';}
+ //      if(!$delete){
+ //        echo 'Delete Error','<br />',mysqli_error($con);}
+ //      else{
+ //        echo 'Delete Down';}
 
-      if(!$update){
-        echo 'Update Error','<br />',mysqli_error($con);}
-      else{
-        echo 'Update Down';}
-      if(!$select){
-        echo 'Select Error','<br />',mysqli_error($con);}
-      else{
-        echo 'Select Down','<br />',mysqli_fetch_assoc($select)['name'];}
+ //      if(!$update){
+ //        echo 'Update Error','<br />',mysqli_error($con);}
+ //      else{
+ //        echo 'Update Down';}
+ //      if(!$select){
+ //        echo 'Select Error','<br />',mysqli_error($con);}
+ //      else{
+ //        echo 'Select Down','<br />',mysqli_fetch_assoc($select)['name'];}
+        echo "用户认证完成";
 ?>
       </body>
 </html>
