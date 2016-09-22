f754299d67f5da145969f981c3a5e757056aaac8
YangChenjie <18092871256@163.com>
2016-09-22 09:16:45 +0800

    complete identity verification;

--- js/index.js
+++
@@ -1,7 +0,0 @@
-function a(){
-	var height=(window.innerHeight-300)/2;
-	document.getElementById('login').style.marginTop=height+'px';
-	// vars a="http://wufazhuce.com/one/"+Math.floor(Math.random()*1000);
-	// document.write(a)
-	// document.body.style.background='url(a)';
-}
---
+++ js/whole.js
@@ -0,0 +1,10 @@
+function login(){	//index.php	==> 登录框
+	var height=(window.innerHeight-300)/2;
+	document.getElementById('login').style.marginTop=height+'px';
+}
+
+function main_height(){
+	var tall=window.innerHeight-18;
+	document.getElementById('main').style.height=tall+'px';
+}
+
